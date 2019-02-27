40597c70c0414c0ed52b54dedb06a556c44a8d33
rodoufu <rodoufu@gmail.com>
2019-02-27 16:33:16 -0300

    Get Eth client only when it is necessary and fixing problem o events form data.

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const methodOverride = require('method-override');
 const EthClient = require('./modules/eth_client');
 
 let ethClient = null;
+function getEthClient() {
+	if (!ethClient) {
+		ethClient = new EthClient();
+	}
+	return ethClient;
+}
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
@@ -12,28 +18,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
 app.get('/api/blockNumber', function (req, res) {
-	if (!ethClient) {
-		ethClient = new EthClient();
-	}
 	console.log('blockNumber begin');
-	ethClient.getBlockNumber((data) => {
+	getEthClient().getBlockNumber((data) => {
 		console.log(data);
 		res.send(JSON.stringify(data));
 	});
     console.log('blockNumber end');
 });
 
-app.get('/api/events/:period/:since', function (req, res) {
-	if (!ethClient) {
-		ethClient = new EthClient();
-	}
+app.post('/api/events', function (req, res) {
 	console.log('events begin');
-	ethClient.getEvents(req.params.period, req.params.since, 0, (err, data) => {
+	getEthClient().getEvents(req.body.period, req.body.since, 0, (err, data) => {
 		if (!err) {
 			console.log("Error: " + err);
 		} else {
