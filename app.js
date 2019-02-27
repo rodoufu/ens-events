b76d2141ff08bf7427cb42c597efcca72c819dd0
rodoufu <rodoufu@gmail.com>
2019-02-27 15:15:37 -0300

    API changes

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const EthClient = require('./modules/eth_client');
 
 let ethClient = null;
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/blockNumber', function (req, res) {
+app.get('/api/blockNumber', function (req, res) {
 	if (!ethClient) {
 		ethClient = new EthClient();
 	}
@@ -20,12 +28,12 @@ app.get('/blockNumber', function (req, res) {
     console.log('blockNumber end');
 });
 
-app.get('/events', function (req, res) {
+app.get('/api/events/:period/:since', function (req, res) {
 	if (!ethClient) {
 		ethClient = new EthClient();
 	}
 	console.log('events begin');
-	ethClient.getEvents("day", 3, 0, (err, data) => {
+	ethClient.getEvents(req.params.period, req.params.since, 0, (err, data) => {
 		if (!err) {
 			console.log("Error: " + err);
 		} else {
